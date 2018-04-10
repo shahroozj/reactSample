067627ae7ace7a9edceeb55dea2b1e77e679d767
Shahrooz Jafari <shahroozj@gmail.com>
2018-04-10 15:52:46 +0800

    Webpack Babel

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import sub , {square, add} from './utils.js';
 import isSenior, {isAdult, canDrink} from './person.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 console.log('app.js is Here!!!');
 console.log(square(6));
@@ -9,3 +11,7 @@ console.log(sub(100, 23));
 console.log(isAdult(18));
 console.log(canDrink(18));
 console.log(isSenior(64));
+
+const template = <p>This is JSX run from webpack</p>
+
+ReactDOM.render(template, document.getElementById('app'));
