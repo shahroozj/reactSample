96953b4248db73293700e8fc03b3c44565e355a8
Shahrooz Jafari <shahroozj@gmail.com>
2018-05-09 14:28:51 +0800

    Fix Wrong Commit 2

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
-        <p>
-            {props.optionText}
-            <button className="button button--link" onClick={(e) => {
-                props.handleDeleteOption(props.optionText);
-                }}
-            >Delete</button>
-        </p>
+    <div className="option">
+        <p className="option__text"> 
+            {props.count}. {props.optionText}
+        </p>    
+        <button className="button button--link" onClick={(e) => {
+            props.handleDeleteOption(props.optionText);
+            }}
+        >Delete</button>
+        
     </div>
 )
 
--- src/components/Options.js
+++ src/components/Options.js
@@ -3,13 +3,17 @@ import Option from './Option';
 
 const Options = (props) => (
     <div>
-        <button className="button button--link" onClick={props.handleDeleteAllOptions}>Remove All</button>
-        {props.options.length === 0 && <p>Please add an item to get started</p>}
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+            <button className="button button--link" onClick={props.handleDeleteAllOptions}>Remove All</button>
+        </div>
+        {props.options.length === 0 && <p className="widget__message">Please add an item to get started</p>}
         {
-            props.options.map((option)  => (
+            props.options.map((option, index)  => (
                 <Option 
                     key={option} 
-                    optionText={option} 
+                    optionText={option}
+                    count={index+1}
                     handleDeleteOption={props.handleDeleteOption}
                 />
             ))
